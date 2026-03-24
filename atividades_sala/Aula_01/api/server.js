c17e685ef0efc381ae4bc2e4ae8204febe07e915
Gabriel Claro <gabriel@MacBook-Air-de-Gabriel.local>
2026-03-24 19:15:38 -0300

    feat: Atividade da aula 01 adicionada

--- atividades_sala/Aula_01/api/server.js
+++ atividades_sala/Aula_01/api/server.js
@@ -13,7 +13,23 @@ let id = 0;
 api.get('/usuarios', (req, res)=>{
     let users = dados;
 
-    res.send(users).status(200)
+    res.status(200).send(users)
+})
+
+
+api.post('/novoUsuario',(req, res)=>{
+    id = id + 1; //incremento
+    
+    let user = {
+        id: id,
+        nome_completo: req.body.nome_completo,
+        email: req.body.email,
+        senha: req.body.senha
+    }
+    dados.push(user)
+    res.status(201).send({
+        code: 201,
+        msg: "Usuário Criado com sucesso!"})
 })
 
 const porta = 8080;
